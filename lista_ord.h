b15e1c048462403eeeff2f1052e570d63292e4e0
guillemandreu <guillem.andreu@students.salle.url.edu>
2018-10-07 23:03:29 +0200

    Fix
    Commit to add the void anadirOrdenado to lista_ord.h

--- lista_ord.h
+++ lista_ord.h
@@ -1,5 +1,5 @@
-#ifndef _LISTA_ORD_H
-#define _LISTA_ORD_H
+#ifndef _LISTA_BID_H
+#define _LISTA_BID_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,40 +7,27 @@
 typedef struct _nodo {
 	int e;
 	struct _nodo * sig;
+	struct _nodo * ant;
 } Nodo;
 
 typedef struct {
 	Nodo * pri;
-	Nodo * ant;
-} ListaOrd;
+	Nodo * ult;
+	Nodo * pdi;
+} ListaBid;
 
-void     LISTAORD_crea (ListaOrd * l);
-//ListaOrd LISTAORD_crea ();
-
-//NO EXISTE PARA EL USUARIO!
-void     LISTAORD_anadir (ListaOrd * l, int e);
-//ListaOrd LISTAORD_anadir (ListaOrd l, int e);
-
-void LISTAORD_anadirOrdenado (ListaOrd * l, int e);
-
-void     LISTAORD_eliminar (ListaOrd * l);
-//ListaOrd LISTAORD_eliminar (ListaOrd l);
-
-void LISTAORD_consultar (ListaOrd l, int * e);
-//int  LISTAORD_consultar (ListaOrd l);
-
-int LISTAORD_vacia (ListaOrd l);
-
-void     LISTAORD_avanzar (ListaOrd * l);
-//ListaOrd LISTAORD_avanzar (ListaOrd l);
-
-int LISTAORD_final (ListaOrd l);
-
-void     LISTAORD_irPrincipio (ListaOrd * l);
-//ListaOrd LISTAORD_irPrincipio (ListaOrd l);
-
-void     LISTAORD_destruye (ListaOrd * l);
-//ListaOrd LISTAORD_destruye (ListaOrd l);
-
-
+ListaBid LISTABID_crea ();
+ListaBid LISTABID_anadirDerecha (ListaBid l, int e);
+ListaBid LISTABID_anadirIzquierda (ListaBid l, int e);
+ListaBid LISTABID_eliminar (ListaBid l);
+int      LISTABID_consultar (ListaBid l);
+int      LISTABID_vacia (ListaBid l);
+int      LISTABID_final (ListaBid l);
+int      LISTABID_principio (ListaBid l);
+ListaBid LISTABID_irPrincipio (ListaBid l);
+ListaBid LISTABID_irFinal (ListaBid l);
+ListaBid LISTABID_avanzar (ListaBid l);
+ListaBid LISTABID_retroceder (ListaBid l);
+ListaBid LISTABID_destruye (ListaBid l);
+ListaBid LISTABID_anadirOrdenado (ListaBid l, int e);
 #endif
