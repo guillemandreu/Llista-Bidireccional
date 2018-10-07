b77aefcad2381d46b29277c8e9ed83a0b0192d33
guillemandreu <guillem.andreu@students.salle.url.edu>
2018-10-07 18:49:38 +0200

    Feat
    I'm stupid and i was working with the file of lista ord from estudy si
    i've copied the correct archive of listabid from estudy

--- lista_ord.c
+++ lista_ord.c
@@ -1,113 +1,177 @@
-#include "lista.h"
+#include "lista_bid.h"
 
-void LISTA_crea (Lista * l) {
-	l->pri = (Nodo*) malloc (sizeof(Nodo));
-	if (l->pri == NULL) {
-		printf("Error al pedir memoria para el fantasma.\n");
+ListaBid LISTABID_crea () {
+	ListaBid l;
+
+	l.pri = NULL;
+	l.pri = (Nodo*) malloc (sizeof(Nodo));
+	if (l.pri == NULL) {
+		printf("Error al pedir memoria para el primer fantasma.\n");
 	}
 	else {
-		l->ant = l->pri;
-		l->pri->sig = NULL;
-	}
-}
-void LISTA_anadir (Lista * l, int e) {
-	Nodo * aux = (Nodo*) malloc (sizeof(Nodo));
-	if (aux == NULL) {
-		printf("Error al pedir memoria para inserir.\n");
-	}
-	else {
-		aux->e = e;
-		aux->sig = l->ant->sig;
-		l->ant->sig = aux;
-		l->ant = l->ant->sig;
-	}
-}
-
-void LISTAORD_anadirOrdenado (ListaOrd * l, int e) {
-	Nodo * aux;
-	aux = l->ant;
-	int encontrado = 0;
-	int elem_consulta;
-
-	LISTAORD_irPrincipio(l);
-	while (!LISTAORD_final(*l) && !encontrado) { //encontrado == 0
-		LISTAORD_consultar(*l, &elem_consulta);
-		if (e < elem_consulta) {
-			encontrado = 1;
+		l.ult = NULL;
+		l.ult = (Nodo*) malloc (sizeof(Nodo));
+		if (l.ult == NULL) {
+			printf("Error al pedir memoria para el segundo fantasma.\n");
+			free(l.pri);
 		}
 		else {
-			LISTAORD_avanzar(l);
+			l.pri->sig = l.ult;
+			l.pri->ant = NULL;
+			
+			l.ult->sig = NULL;
+			l.ult->ant = l.pri;
+
+			l.pdi = l.pri;
 		}
 	}
-	LISTAORD_anadir(l, e);
+
+	return l;
 }
 
-void LISTA_eliminar (Lista * l) {
+
+ListaBid LISTABID_anadirDerecha (ListaBid l, int e) {
 	Nodo * aux;
-	if (l->ant->sig == NULL) {
-		printf("Error. El pdi esta al final de la lista.\n");
-	}
-	else {
-		aux = l->ant->sig;
-		l->ant->sig = l->ant->sig->sig;
-		//l->ant->sig = aux->sig;
-		free(aux);
-	}
-}
-
-void LISTA_consultar (Lista l, int * e) { 
-	if (l.ant->sig == NULL) {
-		printf("Error, estamos en el final de la lista.\n");
-		*e = -1;
-	}
-	else {
-		*e = l.ant->sig->e;
-	}
-}
-
-int  LISTA_vacia (Lista l) {
-	return l.pri->sig == NULL;
-}
-
-void LISTA_avanzar (Lista * l) {
-	if (l->ant->sig == NULL) {
-		printf("Error al avanzar. La lista esta al final.\n");
-	}
-	else {
-		l->ant = l->ant->sig;
-	}
-}
-
-int  LISTA_final (Lista l) {
-	return l.ant->sig == NULL;
-}
-
-void LISTA_irPrincipio (Lista * l) {
-	l->ant = l->pri;
-}
-
-void LISTA_destruir (Lista * l) {
-	//Vaciar la lista de elementos validos
-	LISTA_irPrincipio(l);
-	while (!LISTA_final(*l)) {
-		LISTA_eliminar(l);
-	}
-	//Liberar fantasma
-	free(l->pri); //free(l->ant);
-	l->pri = NULL;
-	l->ant = NULL;
-}
-
-
-/*void LISTA_destruir (Lista * l) {
-	Nodo * aux;
-
-	while (l->pri != NULL) {
-		aux = l->pri;
-		l->pri = l->pri->sig;
-		free(aux);
-	}
 	
-	l->pri = NULL;
-	l->ant = NULL;
-}*/
+	if (l.pdi->sig == NULL) {
+		printf("Error estoy en el ultimo fantasma.\n");
+	}
+	else {
+		aux = (Nodo*) malloc (sizeof(Nodo));
+		if (aux == NULL) {
+			printf("Erro al pedir memoria.\n");
+		}
+		else {
+			aux->e = e;
+			aux->sig = l.pdi->sig;
+			aux->ant = l.pdi;
+			
+			l.pdi->sig->ant = aux;
+			l.pdi->sig = aux;
+		}
+	}
+
+	return l;
+}
+
+ListaBid LISTABID_anadirIzquierda (ListaBid l, int e) {
+	Nodo * aux;
+
+	if (l.pdi->ant == NULL) {
+		printf("Error, no puedo añadir a la izquierda del primer fantasma.\n");
+	}
+	else {
+		aux = (Nodo*) malloc (sizeof(Nodo));
+		if (aux == NULL) {
+			printf("Error al pedir memoria.\n");
+		}
+		else {
+			aux->e = e;
+			aux->sig = l.pdi;
+			aux->ant = l.pdi->ant;
+
+			l.pdi->ant->sig = aux;
+			//(*(*l.pdi).ant).sig = aux;
+
+			l.pdi->ant = aux;
+		}
+	}
+	return l;
+}
+
+
+ListaBid LISTABID_eliminar (ListaBid l) {
+	Nodo * aux;
+
+	if (l.pdi == l.pri || l.pdi == l.ult) {
+		printf("Error, estoy apuntando a un fantasma.\n");
+	}
+	else {
+		aux = l.pdi;
+
+		l.pdi->ant->sig = l.pdi->sig;
+		l.pdi->sig->ant = l.pdi->ant;
+		l.pdi = l.pdi->sig;
+
+		free(aux);
+	}
+
+	return l;
+}
+
+int      LISTABID_consultar (ListaBid l) {
+	int e = -1;
+
+	if (l.pdi == l.pri || l.pdi == l.ult) {
+		printf("Error, estoy apuntando a un fantasma.\n ");
+	}
+	else {
+		e = l.pdi->e;
+	}
+
+	return e;
+}
+
+int      LISTABID_vacia (ListaBid l) {
+	return l.pri->sig == l.ult;	
+	//return l.ult->ant == l.pri;
+}
+
+int      LISTABID_final (ListaBid l) {
+	return l.pdi == l.ult;
+}
+
+int      LISTABID_principio (ListaBid l) {
+	return l.pdi == l.pri;
+}
+
+ListaBid LISTABID_irPrincipio (ListaBid l) {
+	l.pdi = l.pri->sig;
+
+	return l;
+}
+
+ListaBid LISTABID_irFinal (ListaBid l) {
+	l.pdi = l.ult->ant;
+
+	return l;
+}
+
+ListaBid LISTABID_avanzar (ListaBid l) {
+	if (l.pdi->sig == NULL) {
+		printf("Error, estoy en el ultimo fantasma.\n");
+	}
+	else {
+		l.pdi = l.pdi->sig;
+	}
+
+	return l;
+}
+
+ListaBid LISTABID_retroceder (ListaBid l) {
+	if (l.pdi->ant == NULL) {
+		printf("Error, estoy en el primer fantasma.\n");
+	}
+	else {
+		l.pdi = l.pdi->ant;
+	}
+
+	return l;
+}
+
+ListaBid LISTABID_destruye (ListaBid l) {
+	Nodo * aux;
+
+	while (l.pri != NULL) {
+		aux = l.pri;
+		l.pri = l.pri->sig;
+		free(aux);
+	}
+
+	l.pri = NULL;
+	l.ult = NULL;
+	l.pdi = NULL;
+
+	return l;
+}
+
