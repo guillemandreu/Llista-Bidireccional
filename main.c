36673faf1785877b8056635301e8c989aafc3595
guillemandreu <guillem.andreu@students.salle.url.edu>
2018-10-07 18:33:40 +0200

    Feat
    First function calls, if the List doesn't exist it creats it

--- main.c
+++ main.c
@@ -2,12 +2,22 @@
 
 
 
-void main () {
+void main (ListaOrd * 1) {
 
 	int ele;
 	
 		printf ( "Insereix el element que es vol ordenar:\n  ");
 		scanf ("%d%",&ele);
+
+		if ( LISTAORD_vacia(ListaOrd 1) ) {
+
+			LISTAORD_crea( ListaOrd * 1 );
+
+		} else {
+
+			LISTAORD_anadirOrdenado ( ListaOrd * 1, ele );
+
+
 
 }
 
